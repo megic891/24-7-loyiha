3679d876a6489a900fd815ff5d108a82e73b75e9
Islom Annazarov <islomannazarov@MacBook-Air-Islom.local>
2025-11-22 18:03:52 +0500

    added hello route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,42 +1,9 @@
-import {
-  Controller,
-  Get,
-  Param,
-  Post,
-  Body,
-  Put,
-  Delete,
-} from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
   getHello() {
-    return this.appService.getHello();
-  }
-
-  @Get(':id')
-  getById(@Param('id') id: number) {
-    return this.appService.getById(id);
-  }
-
-  @Post()
-  create(@Body() dto: any) {
-    return this.appService.create(dto);
-  }
-
-  @Put(':id')
-  update(@Param('id') id: number, @Body() dto: any) {
-    return this.appService.update(id, dto);
-  }
-
-  
-
-  @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.appService.remove(id);
+    return "Hello Backend!";
   }
 }
